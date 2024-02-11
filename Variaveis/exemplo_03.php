44eaf7d8bd076e3f950ee79de6e6ba2c0b8f2f24
Leo Pavanello <dantix7928@hotmail.com>
2024-02-11 16:45:22 -0300

    Subindo aulas de variaveis

---
+++ Variaveis/exemplo_03.php
@@ -0,0 +1,19 @@
+<?php
+
+$nome = "Hcod";
+$site =  'www.hcode.com.br';
+
+$ano = 1990;
+$salario = 5500.99;
+$bloquado= false;
+//////////////////////////////////////////
+
+$frutas = array("abacaxi", "laranja", "manga");
+
+//echo $frutas[0];
+
+$nascimento = new DateTime();
+
+var_dump($nascimento);
+
+?>
